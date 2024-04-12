c297b7bf439a8a6ede12a3b7bada42d800d08afa
Yash Ghugardare <yghugardare6@gmail.com>
2024-04-12 23:59:21 +0530

    initial setup done

---
+++ app/index.jsx
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import {Link} from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <View className="bg-white flex-1 justify-center items-center" >
+      <Text className="text-3xl">Aura app!</Text>
+      <StatusBar   />
+      <Link href="/profile">Go to Profile</Link>
+    </View>
+  );
+}
+
+
